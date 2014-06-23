46b5caf121a3eba6704d40038fdb61250ac3f4f3
sshumihin <sshumihin@gmail.com>
2014-06-23 20:58:11 +0400

    update for XSI 2014, 2015
    1. fix bat files.
    2. Improve autocomplete ppg. For 3DCoat V3 and V4.
    3. Exporting objects not by scene, by object.

--- Applink_SoftimageXSI/XSI_2013/others/VC92013_x64.bat
+++ Applink_SoftimageXSI/XSI_2013/others/VC92013_x64.bat
@@ -4,7 +4,7 @@ call "c:\Program Files\Autodesk\Softimage 2013 SP1\Application\bin\setenv.bat"
 call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" amd64
 
 set PATH
-set XSISDK_ROOT="c:\Program Files\Autodesk\Softimage 2013 SP1\XSISDK"
+set "XSISDK_ROOT=c:\Program Files\Autodesk\Softimage 2013 SP1\XSISDK"
 
 "c:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" /useenv
 
---
+++ Applink_SoftimageXSI/XSI_2015/others/VC10_XSI2015_x64.bat
@@ -0,0 +1,11 @@
+@echo off
+
+call "c:\Program Files\Autodesk\Softimage 2015\Application\bin\setenv.bat"
+call "e:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
+
+set PATH
+set "XSISDK_ROOT=c:\Program Files\Autodesk\Softimage 2015\XSISDK\"
+
+"e:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" /useenv
+
+echo on
